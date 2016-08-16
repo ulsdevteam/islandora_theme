509416927923bac9f1f2c05e02457055eba818a9
Brian Gillingham <bgilling@pitt.edu>
2016-08-16 11:11:33 -0400

    initial revision for gamera branch

--- templates/page--islandora.tpl.php
+++ templates/page--islandora.tpl.php
@@ -1,33 +1,20 @@
-<div id="pittbar" class="whitebar">
-<a href="http://pitt.edu"><img src="<?php print base_path() . path_to_theme(); ?>/images/pitt-logo-whitebkg.gif" /></a>    <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('footernav')),)); ?>
-</div><!-- /end pittbar-->
-<div id="wrap_headers" class="blue_bg">
+<div id="page-secondary">
+<div class="banner"> <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('banner')),)); ?>
+  <?php if ($logo): ?>
+  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"> <img class="logo" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /> </a>
+  <?php endif; ?>
+  <!-- print banner region --> 
+  <?php print render($page['banner']); ?> </div>
+<!-- /end banner -->
 
-<div class="banner">
-       <img src="/sites/digital/themes/digital_pitt/images/collection_banner.jpg" style="float:right;">
+<div id="nav"> <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('class' => array('primary-nav')),)); ?>
+  <div id="search"> <?php print render($page['search']); ?> </div>
+  <!-- /end search --> 
+</div>
+<!-- /end nav --> 
 
-      <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img class="logo" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
- 
-      <!-- print banner region -->
-        <?php print render($page['banner']); ?>
-    </div>
-<!-- /end banner --> 
-</div> <!-- /end wrap_headers -->
-<div id="wrap_mainmenu" class="ltblue_bg">
-   	<div id="nav">
-			<?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('class' => array('primary-nav')),)); ?>
-      <div id="search">
-        <?php print render($page['search']); ?>
-      </div><!-- /end search -->
-    </div><!-- /end nav -->
-</div><!-- /end wrap_mainmenu -->
 <?php print $messages; ?>
 <div id="two-col-right-main">
-
 
   <?php if ($tabs): ?>
   <div class="tabs"><?php print render($tabs); ?></div>
@@ -37,26 +24,53 @@
     <?php print render($action_links); ?>
   </ul>
   <?php endif; ?>
-  <!--<div id="sidebar">
-    <div class="widget"> <!-- BTB - added 7-15-14 to alleviate the left margin on responsive design media query ~466 in islandora_default.css -->
-      <?php print render($page['sidebar']); ?> 
-    <!-- /end widget -->
+  <?php if ($page['sidebar']): ?>
+  <div id="sidebar">
+    <div class="widget"> <!-- BTB - added 7-15-14 to alleviate the left margin on responsive design media query ~466 in islandora_default.css --> 
+      <?php print render($page['sidebar']); ?> </div>
+    <!-- /end widget --> 
+  </div>
   <!-- /end sidebar -->
-   
+  <?php endif; ?>
   
- 
-  
-  <div id="main-content"> <?php print render($title_prefix); ?>
+  <div id="main-content-outer">
+    <?php if ($solr_search_links && (count($solr_search_links)>0)): ?>
+    <div class="searchnav">
+      <ul class="searchnav-links">
+      <?php foreach ($solr_search_links as $solr_search_link) : ?>
+        <li><?php print $solr_search_link; ?></li>
+      <?php endforeach; ?>
+      </ul>
+      <br class="clearfloat">
+    </div>
+    <?php endif; ?>
+    <?php print render($title_prefix); ?>
     <?php if ($title): ?>
     <h1><?php print $title; ?></h1>
     <?php endif; ?>
-    <?php print render($title_suffix); ?> <?php print render($page['content']); ?> </div>
-  <!-- /end main-content -->
-</div> <!-- /end two-col-right-main -->
-    <div id="footer">
+
+    <?php print render($title_suffix); ?> <?php print render($page['content']); ?>
+
+    <?php if ($collection_img): ?>
+    <div class="islandora-basic-collection-metadata-image"> <?php print $collection_img; ?> </div>
+    <?php endif; ?>
+
+    <?php if ($collection_metadata): ?>
+      <div id="sidebar-right">
+        <div class="islandora-basic-collection-metadata">
+          <?php print $collection_metadata; ?> 
+        </div>
+      </div><!-- /end sidebar -->
+    <?php endif; ?>
+
+  </div>
+  <!-- /end col2 -->
+  <div id="footer">
     <div id="footer-col1"> <?php print render($page['footer-col1']); ?> </div>
     <!-- /end footer column 1 -->
     <div id="footer-col2"> <?php print render($page['footer-col2']); ?> </div>
     <!-- /end footer column 2 --> 
-  </div><!-- /end footer --> 
-
+  </div>
+  <!-- /end footer --> 
+</div>
+<!-- /end page-secondary -->
