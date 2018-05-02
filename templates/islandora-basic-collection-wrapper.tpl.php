15894b7585cd3594dbdedde36a7923e819293da4
Brian Gillingham <bgilling@pitt.edu>
2018-05-02 13:57:13 -0400

    removed some unused variables from the collection view wrapper template - also removed unused template files

--- templates/islandora-basic-collection-wrapper.tpl.php
+++ templates/islandora-basic-collection-wrapper.tpl.php
@@ -1,39 +1,14 @@
-<?php print $messages; ?>
-
 <div id="two-col-left-main">
 
-  <?php if ($tabs): ?>
-  <div class="tabs"><?php print render($tabs); ?></div>
-  <?php endif; ?>
-  <?php if ($action_links): ?>
-  <ul class="action-links">
-    <?php print render($action_links); ?>
-  </ul>
-  <?php endif; ?>
-
   <div id="main-content">
-	<?php print render($title_prefix); ?>
-
-  <div id="metadata-sidebar">
-  <?php if ($collection_metadata): ?>
-  <div class="islandora-basic-collection-metadata">
-    <!--<p>using the islandora metadata framework</p>-->
-    <?php print $collection_metadata; ?> </div>
-  <?php endif; ?>
-                <?php if ($page['sidebar']): ?>
-    <div class="widget"> <?php print render($page['sidebar']); ?> </div>
-    <!-- /end widget -->
-    <?php endif; ?>
-  </div><!-- /end sidebar -->
-
-	<?php if ($collection_img): ?>
-  <div class="islandora-basic-collection-metadata-image"> <?php print $collection_img; ?> </div>
-  <?php endif; ?>
+  <?php print render($title_prefix); ?>
 
   <?php
+    if (module_exists('islandora_datastream_blocks')) {
         $block = block_load('islandora_datastream_blocks', 'idb-DESC');
         $renderable_block = _block_get_renderable_array(_block_render_blocks(array($block)));
         print $renderable_block['islandora_datastream_blocks_idb-DESC']['#markup'];
+    }
   ?>
   <div class="islandora-basic-collection clearfix"> <span class="islandora-basic-collection-display-switch">
     <ul class="links inline">
@@ -42,11 +17,6 @@
       <?php endforeach ?>
     </ul>
     </span>
-    <?php if ($collection_search) {
-      print render($collection_search);
-    } ?>
-    <?php print $collection_pager; ?>
-    <div>Total objects: <?php print $total_count; ?></div>
     <?php print $collection_content; ?>
     <?php print $collection_pager; ?>
   </div><!-- /end islandora-basic-collection-metadata-desc -->
